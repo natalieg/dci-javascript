5acb697d0d46294649c1b9bd666bb80bcf5d3e63
Natalie-Gornicki <natalie.gornicki@gmail.com>
2019-07-18 12:37:46 +0200

    shiny

--- w6/nightsky/script.js
+++ w6/nightsky/script.js
@@ -12,64 +12,45 @@ class Rectangle {
         this.newDiv.style.backgroundColor = this.color;
         this.newDiv.style.margin = "10px";
         this.newDiv.style.position = "absolute";
-        this.newDiv.style.top = getRandomArbitrary(100, 780) + "px";
-        this.newDiv.style.left = getRandomArbitrary(0, 1100) + "px";
+        this.newDiv.style.top = getRandomArbitrary(80, 800) + "px";
+        this.newDiv.style.left = getRandomArbitrary(0, 1200) + "px";
         motherDiv.appendChild(this.newDiv);
     }
 }
 
 class Circle extends Rectangle {
-    constructor(height, width, color, radius){
+    constructor(height, width, color, radius, star){
         super(height, width, color);
         this.radius = radius;
+        this.star = star;
     }
     createCircle(){
         this.create();
         this.newDiv.style.borderRadius = this.radius;
+        if(this.star){
+            this.newDiv.classList.add("shine");
+        }
     }
 }
 
-const c1 = new Circle("50px", "50px", "red", "50%");
-c1.createCircle();
-
-const r1 = new Rectangle("20px", "40px", "green");
-r1.create();
-const r2 = new Rectangle("40px", "80px", "yellow");
-r2.create();
-
-function createRandomDivs(count) {
-    for (i = 0; i < count; i++) {
-        r = new Rectangle(getRandomArbitrary(10, 150) + "px",
-            getRandomArbitrary(10, 150) + "px", getRandomColor());
-        r.create();
-    }
-}
-
-function createRandomCircle(count) {
-    for (i = 0; i < count; i++) {
-         r = new Circle(getRandomArbitrary(10, 150) + "px",
-             getRandomArbitrary(10, 150) + "px", getRandomColor(), "50%");
-        //r = new Circle("50px", "50px", getRandomColor(), "70px");
-        r.createCircle();
-    }
-}
-
-createRandomDivs(30);
-createRandomCircle(30);
+const moon = new Circle("120px", "120px", "#f6de9e", "50%", false);
+moon.createCircle();
+moon.newDiv.classList.add("moon");
 
 let createBtn = document.getElementById("createBtn");
 
 createBtn.addEventListener("click", function(){
     let w = document.getElementById("widthInput").value + "px";
-    let h = document.getElementById("heightInput").value + "px";
+    let h = w;
     let c = document.getElementById("colorInput").value;
     let isCircle = document.getElementById("circleCheck").checked;
+    let isStar = document.getElementById("starCheck").checked;
     console.log(w, h, c, isCircle);
     // If any fild is empty nothing should happen
     if(w != "" && h != "" && c != ""){
         // Creates Circle if checked
         if(isCircle){
-           c = new Circle(h, w, c, "50%");
+           c = new Circle(h, w, c, "50%", isStar);
            c.createCircle();
         } else {
             r = new Rectangle(h, w, c);
