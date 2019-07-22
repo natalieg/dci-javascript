9af69c5ab1bbd1576117ed757dc4407cf6347112
Natalie <natalie.gornicki@gmail.com>
2019-07-22 10:45:55 +0200

    conflict clean

--- w6/nightsky/script.js
+++ w6/nightsky/script.js
@@ -42,7 +42,6 @@ let createBtn = document.getElementById("createBtn");
 createBtn.addEventListener("click", function () {
     let w = document.getElementById("widthInput").value % 20 + "px";
     let h = w;
-<<<<<<< HEAD
     let color = document.getElementById("colorInput").value;
     let amount = document.getElementById('amountInput').value;
     let isCircle = true;
@@ -54,23 +53,8 @@ createBtn.addEventListener("click", function () {
             c = new Circle(h, w, color, "50%", isStar);
             c.createCircle();
             console.log(index, color)
-=======
-    let c = document.getElementById("colorInput").value;
-    let isCircle = true;
-    let isStar = document.getElementById("starCheck").checked;
-    console.log(w, h, c, isCircle);
-    // If any fild is empty nothing should happen
-    if (w != "" && h != "" && c != "") {
-        if(h == "0px"){
-            console.log("zero pixel")
-            h = "2px";
-            w = "2px";
-            console.log(w);
->>>>>>> 224408b362c4f6dbef10832f0d0e306b652610e6
+
         }
-        // Creates Circle if checked
-        c = new Circle(h, w, c, "50%", isStar);
-        c.createCircle();
     } else {
         console.log("Please fill out all filds")
     }
